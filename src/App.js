531e7811e5b321cbca4b963d0a3e65ada31bdd56
AshishChodankar <acchodankar@gmail.com>
2022-01-06 00:33:22 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -13,6 +13,25 @@ function MyComponent() {
     const newList = [...list, "Hello Universe"];
     setList(newList);
   };
-
-  const deleteFirstTweet = () => {};
+  const deleteFirstTweet = () => {
+    list.splice(0, 1);
+    const newList = [...list];
+    setList(newList);
+  };
+  return (
+    <div>
+      <h1>Working with Input Element</h1>
+      <input type="button" value="Tweet to Everyone" onClick={tweetHere} />
+      <input
+        type="button"
+        value="Delete First Tweet"
+        onClick={deleteFirstTweet}
+      />
+      <div id="parent">
+        {list.map((item) => (
+          <div>{item}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
