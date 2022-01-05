5af12bfc9e6431087dc87b8b6ddd4f53fe11e421
AshishChodankar <acchodankar@gmail.com>
2022-01-05 16:13:01 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ export default function App() {
   return (
     <>
       <MyComponent />
-      <MyComponent />
-      <MyComponent />
     </>
   );
 }
 
 function MyComponent() {
-  return <div>This is my component</div>;
+  let username = "rohit";
+  let email = "rohit@gmail.com";
+  let id = 100;
+  let list = ["delhi", "culcutta", "chennai"];
+
+  let obj = { id: 1, name: "rahul" };
+
+  return (
+    <div>
+      <h1>Learning Interpolation {"delhi".toUpperCase()}</h1>
+      <h1>
+        {username}
+        {email}
+        {id}
+      </h1>
+      <h1>{list}</h1>
+      <h1>
+        {obj.id}
+        {obj.name}
+        {`I am also JS ${obj.id} and ${obj.name}`}
+      </h1>
+      <p>Ashish is a brilliant guy.</p>
+    </div>
+  );
 }
