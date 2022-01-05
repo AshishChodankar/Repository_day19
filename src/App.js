eab34f771e02e38164c61f400b7b7795fbea5ddd
AshishChodankar <acchodankar@gmail.com>
2022-01-05 23:33:38 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 export default function App() {
   return (
     <>
@@ -9,27 +8,29 @@ export default function App() {
 }
 
 function MyComponent() {
-  let [counter, setCounter] = useState(0);
-  const increment = () => {
-    counter = counter + 1;
-    setCounter(counter);
+  let [msg, setMsg] = useState("Hello");
+  const changeMessage = (e) => {
+    setMsg(e.target.value);
   };
   return (
     <div>
       <h1>Counter Application</h1>
-      <input type="button" value="Click Me" onClick={increment} />
-      <div>
-        <h1>Like{counter}</h1>
-      </div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
-      <div>Like{counter}</div>
+      <input
+        className="w-100"
+        type="text"
+        name=""
+        id=""
+        value={msg}
+        onChange={changeMessage}
+      />
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
     </div>
   );
 }
